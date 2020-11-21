37364997554d276aa7e38ab40e325b249b510dff
Vickey <vignesh.ramasamyy@gmail.com>
2020-11-21 16:14:24 +0530

    commit 3

--- script/script.js
+++ script/script.js
@@ -3,24 +3,26 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
+  // About
   if (document.documentElement.scrollTop < 380) {
     document.getElementById("at").style.color = "#2C3E50";
   } else document.getElementById("at").style.color = "#808b96";
+
+  //Skills
   if (
     document.documentElement.scrollTop > 380 &&
     document.documentElement.scrollTop < 910
   ) {
-    // document.getElementById("at").style.color = "blue";
-
-    //else
     document.getElementById("ss").style.color = "#2C3E50";
   } else document.getElementById("ss").style.color = "";
 
+  //Portfolio
   if (document.documentElement.scrollTop > 910)
     document.getElementById("po").style.color = "#2C3E50";
   else document.getElementById("po").style.color = "";
 }
 
+// Button action
 function btnact() {
   alert("🚧 Site under construction 🛠️.");
 }
